8af3d92740556b6c34fd152db8f702418ef98eb9
Stefan Thoss <git.identity164@slmails.com>
2025-04-09 15:29:18 -0700

    First working code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# ollama-server-benchmark
+# Ollama Server Benchmark
 Python script for evaluating the performance of Ollama servers using the Ollama API
+
+```shell
+uv run src/main.py -h
+uv run src/main.py -c benchmark.yml -o results.csv -s http://ollama.example.com:11434
+```
---
+++ src/server_response.py
@@ -0,0 +1,25 @@
+MAX_STR_PRINT_LENGTH = 30
+
+class ServerResponse:
+    def __init__(self, response_data, prompt):
+        self.prompt = prompt
+        for key, value in response_data.items():
+            if isinstance(value, str):
+                setattr(self, key, value)
+            elif isinstance(value, int):
+                # All duration fields are using nanoseconds
+                if key.endswith('_duration'):
+                    value = value / 1e9
+                setattr(self, key, value)
+
+        # Calculated fields, see https://github.com/ollama/ollama/blob/main/benchmark/server_benchmark_test.go
+        self.eval_rate = self.eval_count / self.eval_duration
+        self.prompt_eval_rate = self.prompt_eval_count / self.prompt_eval_duration
+
+    def __str__(self):
+        attrs = []
+        for key, value in vars(self).items():
+            if isinstance(value, str) and len(value) > MAX_STR_PRINT_LENGTH:
+                value = value[:MAX_STR_PRINT_LENGTH] + '...'
+            attrs.append(f"{key}={value}")
+        return f"ServerResponse({', '.join(attrs)})"

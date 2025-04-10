f0cfdf05aad8530f14a94c31e2af449768bda247
Stefan Thoss <git.identity164@slmails.com>
2025-04-09 21:51:31 -0700

    Unloading functionality

--- src/main.py
+++ src/main.py
@@ -12,14 +12,14 @@ from server_response import ServerResponse
 LOGGER = structlog.get_logger()
 
 
-def main(ollama_server, config_file, output_csv):
+def main(ollama_server, config_file, output_csv, skip_unloading):
     with open(config_file, "r") as f:
-        data = yaml.safe_load(f)
+        config = yaml.safe_load(f)
 
     if (
-        not all(key in data for key in ["models", "prompts"])
-        or len(data["models"]) < 1
-        or len(data["prompts"]) < 1
+        not all(key in config for key in ["models", "prompts"])
+        or len(config["models"]) < 1
+        or len(config["prompts"]) < 1
     ):
         raise ValueError(
             "Config YAML file must contain at least one model and one prompt"
@@ -35,15 +35,41 @@ def main(ollama_server, config_file, output_csv):
     LOGGER.info(f"Ollama server version: {response.json()['version']}")
 
     # Check that all models exist on server
-    for model in data["models"]:
-        try:
-            payload = {"model": model, "verbose": False}
-            response = requests.post(f"{ollama_server}/api/show", json=payload)
-            response.raise_for_status()
-        except requests.exceptions.RequestException as e:
-            LOGGER.debug(f"Did not find model {model} on Ollama server.", error=str(e))
+    try:
+        response = requests.get(f"{ollama_server}/api/tags")
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        LOGGER.error(f"Could not load model list from Ollama server", error=str(e))
+        sys.exit(1)
+    json_response = response.json()
+    local_models = [obj['name'] for obj in json_response["models"]]
+    LOGGER.debug(f"Models available on Ollama server: {local_models}")
+    for model in config["models"]:
+        if model not in local_models:
+            LOGGER.error(f"Did not find model {model} on Ollama server")
             sys.exit(1)
-        LOGGER.info(f"Found model {model} on Ollama server.")
+    LOGGER.info(f"All configured models available on Ollama server")
+
+    # Unload all running models
+    if not skip_unloading:
+        try:
+            response = requests.get(f"{ollama_server}/api/ps")
+            response.raise_for_status()
+            json_response = response.json()
+            running_models = [obj['name'] for obj in json_response["models"]]
+            if len(running_models) > 0:
+                LOGGER.debug(f"Models running on Ollama server: {running_models}")
+                for model in running_models:
+                    payload = {
+                        "model": model,
+                        "keep_alive": 0,
+                    }
+                    response = requests.post(f"{ollama_server}/api/generate", json=payload)
+                    response.raise_for_status()
+            LOGGER.info("No models running on Ollama server")
+        except requests.exceptions.RequestException as e:
+            LOGGER.error(f"Could not unload running models from Ollama server", error=str(e))
+            sys.exit(1)
 
     result_cols = [
         "model",
@@ -57,7 +83,7 @@ def main(ollama_server, config_file, output_csv):
     results = [result_cols]
 
     # Request prompts
-    for model, prompt in product(data["models"], data["prompts"]):
+    for model, prompt in product(config["models"], config["prompts"]):
         try:
             payload = {
                 "model": model,
@@ -107,6 +133,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-o", "--output_csv", default="results.csv", help="Path to output CSV file"
     )
+    parser.add_argument('--skip-unloading', action='store_true')
     args = parser.parse_args()
 
-    main(args.ollama_server, args.config_file, args.output_csv)
+    main(args.ollama_server, args.config_file, args.output_csv,  args.skip_unloading)
